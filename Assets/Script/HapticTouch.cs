b1801ea5b94b989f220087fa693fb2781b47eed0
Thairan Chowdhury <thairanchowdhury@thairans-mbp.eduroam.student>
2019-05-17 16:24:01 +0100

    initial commit

---
+++ Assets/Script/HapticTouch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HapticTouch : MonoBehaviour {
+
+
+    public bool hapticFlag = false;
+    private SteamVR_TrackedObjecttrackerObject;
+        private SteamVR_Controller.Device device;
+
+   void Start()
+    {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    void Update()
+
+    {
+        device = SteamVR_Controller.Input((int)trackedObject.index);
+        if(hapticFlag)
+        {
+            device.TriggerHapticPulse(600);
+    }
+}
+		void OnTriggerEnter () {
+        hapticFlag = true;
+	}
+    private void OnTriggerExit()
+    {
+        hapticFlag = false;
+    }
+}
